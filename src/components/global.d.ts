9d59f4821de81c12cd2589904bdb35b8e345f5ee
Aman Tiwari <amantiwari8861@gmail.com>
2026-04-07 13:57:50 +0530

    removed duplicate product types

--- src/components/global.d.ts
+++ src/components/global.d.ts
@@ -2,15 +2,6 @@ declare module 'swiper/css';
 declare module 'swiper/css/navigation';
 declare module 'swiper/css/pagination';
 
-type Product = {
-  id: number;
-  title: string;
-  image: string;
-  price: number;
-  mrp: number;
-  discount: number;
-  deliveryDate: string;
-};
 type User = {
   _id: string;
   name: string;
